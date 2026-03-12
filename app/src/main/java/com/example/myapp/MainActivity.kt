e0111f29c53433348586fdac95566a1772cfbe45
GschYuliya <gushchinayuliyaa@gmail.com>
2026-03-12 21:54:34 +0500

    Update MainActivity.kt

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        // Загружаем сохранённый язык
         loadLocale()
 
         super.onCreate(savedInstanceState)
